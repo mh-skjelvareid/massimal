0708e13225e5d57d9b7deaa136d186a7f06c8583
Martin H. Skjelvareid <martin.hansen.skjelvareid@gmail.com>
2021-12-03 12:53:48 +0100

    Testing glint correction

--- tools/preprocess.py
+++ tools/preprocess.py
@@ -3,6 +3,7 @@
 # Imports
 import cv2
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
 # detect_saturated
 def detect_saturated(image,sat_val=2**12-1,src_ax=2):
@@ -122,3 +123,150 @@ def remove_glint_flatspec(image,wl,nir_band=(780,840)):
     image_noglint = image - mean_nir
 
     return image_noglint
+
+
+def train_hedley_sun_glint(spec,wl,vis_band=(350,750),nir_band=(780,840)):
+    """ Train Hedley sun glint removal regression model
+
+    # Usage:
+    glint_reg = train_hedley_sun_glint(spec,wl,...)
+
+    # Required arguments:
+    spec:   2D numpy array with hyperspectral image, wavelengths along 2nd dim.
+            The cube should contain data from a homogenous bottom (e.g. deep sea)
+            and a representative variation of sun/sky glint.
+    wl:     1D array of wavelenghs (numeric). Must match the size of the 2nd
+            dimension of spec.
+
+    # Optional arguments:
+    vis_band:   2-element tuple with upper and lower wavelength of VIS band (nm).
+    nir_band:   2-element tuple with upper and lower limit of NIR band.
+                The NIR band is used to estimate the amount of sun glint present
+                in the VIS band.
+
+    # Returns:
+    glint_reg:
+
+    # Notes:
+    This function is based on the following paper: Hedley, J. D., Harborne, A.
+    R., & Mumby, P. J. (2005). Technical note: Simple and robust removal of sun
+    glint for mapping shallow‐water benthos. International Journal of Remote
+    Sensing, 26(10), 2107–2112. https://doi.org/10.1080/01431160500034086
+
+    """
+
+    # Calculate VIS and NIR indices
+    vis_ind = (wl >= vis_band[0]) & (wl <= vis_band[1])
+    nir_ind = (wl >= nir_band[0]) & (wl <= nir_band[1])
+
+    # Calculate mean NIR value in band
+    nir = np.mean(spec[:,nir_ind], keepdims=True)
+
+    # Extract VIS data
+    vis = spec[:,vis_band]
+
+    # Fit a linear regression model
+    reg = LinearRegression().fit(nir, vis)
+
+    # Estimate minimum NIR value as 2nd percentile (more robust than strict minimum)
+    min_nir = np.percentile(nir,2)
+
+    # The intercept value should be set to zero
+
+    return 0
+
+
+class HedleySunGlint:
+    """ Hedley sun glint removal regression model
+
+    # Usage:
+    GlintMod = HedleySunGlint()
+
+    # Notes:
+    This implementation is based on the following paper: Hedley, J. D.,
+    Harborne, A. R., & Mumby, P. J. (2005). Technical note: Simple and robust
+    removal of sun glint for mapping shallow‐water benthos. International
+    Journal of Remote Sensing, 26(10), 2107–2112.
+    https://doi.org/10.1080/01431160500034086
+
+    """
+
+    def __init__(self, vis_band = (350,750), nir_band = (780,840)):
+        """ Hedley sun glint removal: Model instantiation
+
+        # Optional arguments:
+        vis_band:   2-element tuple with upper and lower wavelength of VIS band (nm).
+        nir_band:   2-element tuple with upper and lower limit of NIR band.
+                    The NIR band is used to estimate the amount of sun glint present
+                    in the VIS band.
+        """
+        self.vis_band = vis_band
+        self.nir_band = nir_band
+
+
+    def fit(self,spec,wl):
+        """ Fit a linear regression model connecting NIR and VIS data
+
+        # Required arguments:
+        spec:   2D numpy array with hyperspectral image, wavelengths along 2nd dim.
+                The cube should contain data from a homogenous bottom (e.g. deep sea)
+                and a representative variation of sun/sky glint.
+        wl:     1D array of wavelenghs (numeric). Must match the size of the 2nd
+                dimension of spec.
+
+        """
+
+        # Calculate VIS and NIR indices
+        self.wl = wl
+        self.vis_ind = (wl >= self.vis_band[0]) & (wl <= self.vis_band[1])
+        self.nir_ind = (wl >= self.nir_band[0]) & (wl <= self.nir_band[1])
+
+        # Calculate mean NIR value in band
+        nir = np.mean(spec[:,self.nir_ind], axis=1, keepdims=True)
+
+        # Extract VIS data
+        vis = spec[:,self.vis_ind]
+
+        # Fit a linear regression model
+        reg = LinearRegression().fit(nir, vis)
+        self.b = np.reshape(reg.coef_,(1,-1))   # Row vector, for mat. mult.
+
+        # Estimate minimum NIR value as 2nd percentile (more robust than strict minimum)
+        self.min_nir = np.percentile(nir,2)
+
+
+    def remove_glint(self,data):
+        """ Remove sun glint by applying previously trained model
+
+        # Required arguments:
+        data:   2D or 3D spectral data, wavelengths along last dim.
+                The wavelengths must match those of the training data unsed
+                to fit the model.
+
+        # Returns:
+        vis:    2D or 3D spectral data, limited to visible range, with sun
+                glint removed.
+        """
+
+        # Shape into 2D array, save original shape for later
+        input_shape = data.shape
+        data = np.reshape(data,(-1,data.shape[-1]))
+
+        # Extract VIS and NIR bands
+        vis = data[:,self.vis_ind]
+        nir = np.mean(data[:,self.nir_ind], axis=1, keepdims=True)
+
+        # Offset NIR, taking into account "ambient" (minimum) NIR
+        # NOTE: This may be unnecessary for UAV data (minimal atmosphere effects)?
+        nir = nir - self.min_nir
+        nir[nir < 0] = 0;
+
+        # Estimate sun glint in VIS range and subtract it
+        vis = vis - nir @ self.b    # Matrix mult. with slope b for each VIS band
+
+        # Reshape data to fit original dimensions
+        output_shape = input_shape[:-1] + (vis.shape[-1],)
+        vis = np.reshape(vis,output_shape)
+
+        # Return
+        return vis
