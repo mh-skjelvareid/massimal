3f1a6121a45aebf1b8ccfb0515c438f21dfd19b3
Martin Skjelvareid <martin.hansen.skjelvareid@gmail.com>
2023-01-13 14:11:12 +0100

    Reorganizing preliminary Jupyter files into folders

--- prelim/quickstart_fiftyone.py
+++
@@ -1,8 +0,0 @@
-import fiftyone as fo
-import fiftyone.zoo as foz
-
-dataset = foz.load_zoo_dataset("quickstart")
-session = fo.launch_app(dataset, port=5151)
-
-# Blocks execution until the App is closed
-session.wait()
