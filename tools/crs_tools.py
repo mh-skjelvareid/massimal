f4020c9a7117543768e9abda3ebdb188b6c81857
mh-skjelvareid <martin.hansen.skjelvareid@gmail.com>
2024-02-23 15:56:46 +0100

    Adding function to convert between long/lat and utm coordinates

--- tools/crs_tools.py
+++ tools/crs_tools.py
@@ -1,6 +1,7 @@
 import geopandas as gpd
 import pandas as pd
 import shapely
+import pyproj
 
 
 def convert_gcp_to_new_crs(gcp_csv_path_orig,gcp_csv_path_new,delimiter=';',crs_old='EPSG:4326',convert_to_utm=True,crs_new=None):
@@ -36,3 +37,41 @@ def convert_gcp_to_new_crs(gcp_csv_path_orig,gcp_csv_path_new,delimiter=';',crs_
 
     # Save to file
     gdf_new.to_csv(gcp_csv_path_new)
+
+
+def convert_long_lat_to_utm(long,lat,return_utm_epsg=False):
+    """ Convert longitude and latitude coordinates (WGS84) to UTM 
+    
+    # Input parameters:
+    long:
+        Longitude coordinate(s), scalar or array
+    lat:
+        Latitude coordinate(s), scalar or array
+    return_utm_epsg:
+        The UTM zone is automatically estimated based on coordinates.
+        If return_utm_epsg=True, the EPSG code for the UTM zone is returned
+
+    Returns:
+    UTMx:
+        UTM x coordinate ("Easting"), scalar or array
+    UTMx:
+        UTM x coordinate ("Easting"), scalar or array
+    UTM_epsg (only if return_utm_epsg=True):
+        EPSG code (integer) for UTM zone    
+    """
+    utm_crs_list = pyproj.database.query_utm_crs_info(
+        datum_name="WGS 84",
+        area_of_interest = pyproj.aoi.AreaOfInterest(
+            west_lon_degree=min(long),
+            south_lat_degree=min(lat),
+            east_lon_degree=max(long),
+            north_lat_degree=max(lat),
+        ),
+    )
+    utm_crs = pyproj.CRS.from_epsg(utm_crs_list[0].code)
+    proj = pyproj.Proj(utm_crs)
+    UTMx, UTMy = proj(lcf_long,lcf_lat)
+    if return_utm_epsg:
+        return UTMx, UTMy, utm_crs.to_epsg()
+    else:
+        return UTMx, UTMy
