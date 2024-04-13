927c878565cd8d159d28f891a2b04c9bbf275ed2
mh-skjelvareid <martin.hansen.skjelvareid@gmail.com>
2024-04-13 20:56:47 +0200

    Adding code for grouping classes based on hierarchy (tree structure)

--- src/massimal/annotation.py
+++ src/massimal/annotation.py
@@ -1,11 +1,10 @@
-# preprocess
-
 # Imports
 import json
 import skimage.io
 import numpy as np
 import json
 import copy
+import warnings
 
 def read_hasty_metadata(hasty_json):
     """ Read class names and indices for Hasty.ai annotation
@@ -310,3 +309,88 @@ def annotation_data_to_matrix(data,class_dict):
             ind = ind + n_samp_loc
 
     return X,y
+
+
+
+def class_indices_from_hierarchy(class_hierarchy:dict,class_indices:dict[str,int],class_names:list[str]) -> dict[str,list[int]]:
+    """ Group class indices based on class hierarchy 
+    
+    # Input arguments:
+    class_hierarchy: dict
+        See massimal_annotation_class_hierarchy.json
+        Nested dictionary, with outer levels being more general and inner levels
+        being more specific. Lowest level is always a list, but the list can be empty.
+    class_indices : dict
+        Dictionary (not nested) with class names as keys and class indices 
+        (for masks / y-vectors) as values.
+    class_names: list
+        List of grouped class names. The names must correspond a name in the 
+        class hierarchy. If the name is part of a list at the lowest level (leaf),
+        the corresponding index is added to the indices for the group.
+        If the name is a subcategory (branch), the index for the name and for all
+        classes belonging to the subcategory are added to the group indices.
+        Class names should not overlap, i.e., one class in the list should not be a 
+        subcategory of another class in the list. However, this is not checked
+        by the code.
+
+    # Returns:
+    grouped_class_indices: dict
+        Dictionary with grouped class names as keys and list of corresponding class indices
+        as values.
+
+    # Example:
+    class_hierarchy = {'Vegetation':{'Grass':[],'Trees':['Oak','Birch']},
+        'Rock':['Bedrock','Cobble'],'Buildings':[]}
+    class_indices = {'Vegetation':1,'Grass':2,'Trees':3,'Oak':4,'Birch':5,
+        'Rock':6,'Bedrock':7,'Cobble':8,'Buildings':9}
+    
+    With class_names = ['Grass','Trees','Bedrock','Cobble','Buildings'], 
+    class_indices_from_hierarchy(class_hierarchy,class_indices,class_names) returns
+        {'Grass':[2],'Trees':[3,4,5],'Bedrock':[7],'Cobble':[8],'Buildings':[9]}
+    
+    """
+    grouped_class_indices = {}
+    for class_name in class_names:
+        # Check if class name is present in class_indices
+        if class_name not in class_indices:
+            warnings.warn(f'Class name "{class_name}" is not included in class_indices.')
+            continue
+        
+        # Get indices corresponding to class name
+        grouped_class_indices[class_name] = _class_tree_search(class_hierarchy,class_indices,class_name)
+        
+        # Issue warning if class name not found in class hierarchy
+        if not(grouped_class_indices[class_name]):
+            warnings.warn(f'Class name "{class_name}" not found in class_hierarchy')
+    return grouped_class_indices
+
+
+def _collect_all_class_branch_indices(class_branch:dict, class_indices:dict) -> list[int]:
+    """ Recursively collect all class indices of a given class branch """
+    branch_indices = []
+    for current_class_name in class_branch:
+        if current_class_name in class_indices:
+            branch_indices.append(class_indices[current_class_name])  # Include class indices for current level
+        else:
+            warnings.warn(f'Class name "{current_class_name}" in class_hierarchy not present as key in class_indices.')
+        if isinstance(class_branch,dict):                         # If dictionary (nested),
+            branch_indices += _collect_all_class_branch_indices(   # recursively collect all "child" indices
+                class_branch[current_class_name],class_indices)
+    return branch_indices
+
+
+def _class_tree_search(class_tree,class_indices:dict,class_name:str) -> list[int]:
+    """ Recursively get class indices for given class name, with subclasses"""
+    group_indices = []
+
+    for current_class_name in class_tree:
+        if group_indices: # If we already found the class we're searching for, stop
+            break
+        if current_class_name == class_name:                # If class name found,
+            group_indices.append(class_indices[class_name]) # add index for class, and
+            if isinstance(class_tree,dict):                 # if subclasses are present, add them as well
+                group_indices += _collect_all_class_branch_indices(class_tree[current_class_name],class_indices)
+        else:                                               # If class name not found at this level,
+            if isinstance(class_tree,dict):                 # search one level down (if possible)
+                group_indices = _class_tree_search(class_tree[current_class_name],class_indices,class_name)
+    return group_indices
