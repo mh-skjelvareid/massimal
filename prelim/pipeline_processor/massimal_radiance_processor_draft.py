3b8e5f2fef92f2deb2a99724a4f25679aedb892d
mh-skjelvareid <martin.hansen.skjelvareid@gmail.com>
2024-06-16 23:01:14 +0200

    Update massimal_radiance_processor_draft.py

--- prelim/pipeline_processor/massimal_radiance_processor_draft.py
+++ prelim/pipeline_processor/massimal_radiance_processor_draft.py
@@ -9,20 +9,29 @@ def read_envi(header_filename: Union[Path,str],
               image_filename: Union[Path,str,None]=None) -> tuple[np.ndarray,np.ndarray,dict]:
     """ Load image in ENVI format, including wavelength vector and other metadata
 
-    # Usage:
+    Usage:
+    ------
     (image,wl,rgb_ind,metadata) = read_envi(header_filename,...)
 
-    # Required arguments:
-    header_filename: Path to ENVI file header.
+    Arguments:
+    ----------
+    header_filename: Path | str
+        Path to ENVI file header.
 
-    # Optional arguments:
-    image_filename: Path to ENVI data file, useful if data file is not found
-                    automatically from header file name (see spectral.io.envi.open).
+    Keyword arguments:
+    ------------------
+    image_filename: Path | str
+        Path to ENVI data file, useful if data file is not found
+        automatically from header file name (see spectral.io.envi.open).
 
     Returns:
-    image:      ndarray with shape (n_lines, n_samples, n_channels) 
-    wl:         Wavelength vector (float)
-    metadata:   Image metadata (dictionary). 
+    --------
+    image: np.ndarray
+        image, shape (n_lines, n_samples, n_channels) 
+    wl: np.ndarray
+        Wavelength vector, shape (n_channels,) 
+    metadata:  dict
+        Image metadata (ENVI header content). 
     """
 
     # Open image handle
@@ -39,8 +48,47 @@ def read_envi(header_filename: Union[Path,str],
 
 
 class RadianceCalibrationDataset:
+    """ A radiance calibration dataset for Resonon hyperspectral cameras.
+    
+    Attributes:
+    -----------
+    calibration_file: Path
+        Path to Imager Calibration Pack (*.icp) file
+    calibration_dir: Path
+        Folder into which all the data in the calibration_file
+        is put (unzipped).
+    gain_file_path: Path
+        Path to "gain file", i.e. ENVI file with "gain spectra", 
+        per-wavelength values for converting from raw (digital numbers) 
+        data to radiance data (in physical units).
+    dark_frame_paths: list[Path]
+        List of paths to "dark frames", i.e. files with a single line / spectrum
+        taken with no light incident on the sensor, taken with different gain
+        and shutter values. 
+
+    Methods:
+    --------
+    get_radiance_conversion_frame():
+        Returns radiance conversion frame, shape (n_samples, n_channels)
+    get_closest_dark_frame(gain,shutter)
+        Returns dark frame which best matches given gain and shutter values
+    
+    """
 
     def __init__(self, calibration_file: Union[Path,str]):
+        """ Un-zip calibration file and create radiance calibration dataset 
+        
+        Arguments:
+        ----------
+        calibration_file: Path | str
+            Path to *.icp Resonon "Imager Calibration Pack" file
+
+        Raises:
+        -------
+        ValueError:
+            If given *.icp file is not a valid zip file
+        
+        """
         # Register image calibration "pack" (*.icp) and check that it exists
         self.calibration_file = Path(calibration_file)
         assert self.calibration_file.exists()
@@ -80,8 +128,8 @@ class RadianceCalibrationDataset:
             dark_frame_gains.append(int(gain_str[:-4]))
             dark_frame_shutters.append(int(shutter_str[:-7]))
         # Save as NumPy arrays
-        self.dark_frame_gains = np.array(dark_frame_gains,dtype=np.float)
-        self.dark_frame_shutters = np.array(dark_frame_shutters,dtype=np.float)
+        self._dark_frame_gains = np.array(dark_frame_gains,dtype=np.float)
+        self._dark_frame_shutters = np.array(dark_frame_shutters,dtype=np.float)
 
             
     def _get_closest_dark_frame_path(self, 
@@ -90,30 +138,66 @@ class RadianceCalibrationDataset:
                                ) -> Path:
         """ Search for dark frame with best matching gain and shutter values """
         # First search for files with closest matching gain
-        candidate_gains = np.unique(self.dark_frame_gains)
+        candidate_gains = np.unique(self._dark_frame_gains)
         closest_gain = candidate_gains[np.argmin(abs(candidate_gains - gain))]
         
         # Then search (in subset) for single file with closest matching shutter 
-        candidate_shutters = np.unique(self.dark_frame_shutters[self.dark_frame_gains == closest_gain])
-        closest_shutter = self.dark_frame_shutters[np.argmin(abs(candidate_shutters - shutter))]
+        candidate_shutters = np.unique(self._dark_frame_shutters[self._dark_frame_gains == closest_gain])
+        closest_shutter = self._dark_frame_shutters[np.argmin(abs(candidate_shutters - shutter))]
         
         # Return best match
-        best_match_mask = (self.dark_frame_gains==closest_gain) & (self.dark_frame_shutters==closest_shutter)
+        best_match_mask = (self._dark_frame_gains==closest_gain) & (self._dark_frame_shutters==closest_shutter)
         best_match_ind = np.nonzero(best_match_mask)
         assert len(best_match_ind) == 1 # There should only be a single best match
-        return self.dark_frame_paths[best_match_ind]
+        return self.dark_frame_paths[best_match_ind], closest_gain, closest_shutter
         
-
     def get_closest_dark_frame(self, 
                                gain: Union[int,float], 
                                shutter: Union[int,float]
-                               ) -> tuple[np.ndarray, np.adarray, dict]:
-        return read_envi(self.get_closest_dark_frame_path(gain,shutter))
+                               ) -> tuple[np.ndarray, np.ndarray, dict, float, float]:
+        """ Get dark frame which most closely matches given gain and shutter values 
+        
+        Arguments:
+        ----------
+        gain:
+            Gain value used for search, typically gain value of image that should
+            be converted from raw to radiance. Values follow Resonon convention 
+            used in header files++ (logarithmic values, 20 log10).
+        shutter:
+            Shutter value used for search, typically shutter value of image that should
+            be converted from raw to radiance. Values follow Resonon convention 
+            used in header files++ (unit: milliseconds).
+
+        Returns:
+        --------
+        frame: np.ndarray
+            TODO: Check shape
+        wl: np.ndarray 
+            Vector of wavelengths for each spectral channel (nanometers)
+        metadata: dict
+            Metadata from ENVI header, formatted as dictionary
+        """
+        closest_file, closest_gain, closest_shutter = self._get_closest_dark_frame_path()
+        frame, wl, metadata = read_envi(closest_file)
+        return frame, wl, metadata, closest_gain, closest_shutter
 
 
-    def get_radiance_conversion_frame(self) -> tuple[np.ndarray, np.adarray, dict]:
-        """ Read and return radiance conversion curve ("gain" file) """
+    def get_radiance_conversion_frame(self) -> tuple[np.ndarray, np.ndarray, dict]:
+        """ Read and return radiance conversion curve ("gain" file) 
+        
+        Returns:
+        --------
+        frame: np.ndarray
+            TODO: Check shape
+        wl: np.ndarray 
+            Vector of wavelengths for each spectral channel (nanometers)
+        metadata: dict
+            Metadata from ENVI header, formatted as dictionary
+        
+        """
         return read_envi(self.gain_file_path)  
+
+
 
 
 class RadianceProcessor:
