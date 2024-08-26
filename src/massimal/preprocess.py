8b723c0bb01618543d4809ee938c2cc3d6f33078
mh-skjelvareid <martin.hansen.skjelvareid@gmail.com>
2024-08-26 09:52:56 +0200

    Black formatting + comments on Hedley SGC

--- src/massimal/preprocess.py
+++ src/massimal/preprocess.py
@@ -3,14 +3,14 @@
 # Imports
 import cv2
 import numpy as np
-from sklearn.linear_model import LinearRegression
 import skimage.morphology
 from scipy.signal import savgol_filter
+from sklearn.linear_model import LinearRegression
 
 
 # detect_saturated
-def detect_saturated(image,sat_val=2**12-1,src_ax=2):
-    """ Detect saturated pixels in single- or multiband image
+def detect_saturated(image, sat_val=2**12 - 1, src_ax=2):
+    """Detect saturated pixels in single- or multiband image
 
     # Usage:
     mask = detect_saturated(image,...)
@@ -28,13 +28,13 @@ def detect_saturated(image,sat_val=2**12-1,src_ax=2):
             above sat_val.
     """
 
-    mask = np.any( np.atleast_3d(image) >= sat_val, axis = src_ax)
+    mask = np.any(np.atleast_3d(image) >= sat_val, axis=src_ax)
     return mask
 
 
 # inpaint_masked
-def inpaint_masked(in_im, mask, inpaint_radius=3, inpaint_alg = 'ns', verbose=False):
-    """ Inpaint masked pixels in all bands of multiband image
+def inpaint_masked(in_im, mask, inpaint_radius=3, inpaint_alg="ns", verbose=False):
+    """Inpaint masked pixels in all bands of multiband image
 
     # Usage:
     out_im = inpaint_masked(in_im,mask,...)
@@ -82,24 +82,30 @@ def inpaint_masked(in_im, mask, inpaint_radius=3, inpaint_alg = 'ns', verbose=Fa
     assert mask.shape[0:2] == in_im.shape[0:2]
 
     # Convert to OpenCV inpaint algorithm flags
-    if inpaint_alg.lower() == 'ns':
+    if inpaint_alg.lower() == "ns":
         alg_flag = cv2.INPAINT_NS
-    elif inpaint_alg.lower() == 'telea':
+    elif inpaint_alg.lower() == "telea":
         alg_flag = cv2.INPAINT_TELEA
     else:
-        raise ValueError("inpaint_alg must be \'ns\' or \'telea\'")
+        raise ValueError("inpaint_alg must be 'ns' or 'telea'")
 
     # Loop over each image band and apply inpainting
     for ii in range(out_im.shape[2]):
-        if verbose: print('Inpainting band ' + str(ii+1) + ' of ' + str(out_im.shape[2]),end="\r")
-        out_im[:,:,ii] = cv2.inpaint( out_im[:,:,ii],mask,inpaint_radius,alg_flag)
+        if verbose:
+            print(
+                "Inpainting band " + str(ii + 1) + " of " + str(out_im.shape[2]),
+                end="\r",
+            )
+        out_im[:, :, ii] = cv2.inpaint(out_im[:, :, ii], mask, inpaint_radius, alg_flag)
 
     # Return
     return out_im
 
 
-def inpaint_missing_pixels(image, inpaint_radius=3, binary_closing_radius=3, inpaint_alg='ns'):
-    """ Inpaint missing pixels in all bands of multiband image
+def inpaint_missing_pixels(
+    image, inpaint_radius=3, binary_closing_radius=3, inpaint_alg="ns"
+):
+    """Inpaint missing pixels in all bands of multiband image
 
     # Arguments:
     image:  2D or 3D image array. Multiband images are assumed to have bands
@@ -124,25 +130,24 @@ def inpaint_missing_pixels(image, inpaint_radius=3, binary_closing_radius=3, inp
     the whole image (typically, georeferenced images have large border areas with
     zero-valued pixels).
     """
-    
+
     # Determine which pixels to inpaint
     image = np.atleast_3d(image)
-    zero_mask = np.all(image==0,axis=2)
-    nonzero_mask_holes_filled = skimage.morphology.binary_closing(~zero_mask,
-                                    footprint=skimage.morphology.disk(radius=binary_closing_radius))
+    zero_mask = np.all(image == 0, axis=2)
+    nonzero_mask_holes_filled = skimage.morphology.binary_closing(
+        ~zero_mask, footprint=skimage.morphology.disk(radius=binary_closing_radius)
+    )
     inpaint_mask = zero_mask & nonzero_mask_holes_filled
 
     # Inpaint zero pixels inside image
-    inpainted_image = inpaint_masked(image, 
-                                     inpaint_mask, 
-                                     inpaint_radius=inpaint_radius, 
-                                     inpaint_alg = inpaint_alg)
+    inpainted_image = inpaint_masked(
+        image, inpaint_mask, inpaint_radius=inpaint_radius, inpaint_alg=inpaint_alg
+    )
     return inpainted_image
 
 
-
-def remove_glint_flatspec(image,wl,nir_band=(780,840),ignore_band=(753,773)):
-    """ Remove sun/sky glint assuming flat glint spectrum
+def remove_glint_flatspec(image, wl, nir_band=(780, 840), ignore_band=(753, 773)):
+    """Remove sun/sky glint assuming flat glint spectrum
 
     # Usage:
     image_noglint = remove_glint_flatspec(image,wl,...)
@@ -152,7 +157,7 @@ def remove_glint_flatspec(image,wl,nir_band=(780,840),ignore_band=(753,773)):
     wl:     1D array of wavelenghs (numeric)
 
     # Optional arguments:
-    nir_band:   
+    nir_band:
         2-element tuple with upper and lower limit of NIR band.
         The average value of the NIR band is subtracted from the
         original image.
@@ -171,15 +176,14 @@ def remove_glint_flatspec(image,wl,nir_band=(780,840),ignore_band=(753,773)):
     nir_ind = (wl > nir_band[0]) & (wl < nir_band[1]) & (~ignore_ind)
 
     # Subtract average NIR value from all of image.
-    mean_nir = np.mean(image[:,:,nir_ind], axis=2, keepdims=True)
+    mean_nir = np.mean(image[:, :, nir_ind], axis=2, keepdims=True)
     image_noglint = image - mean_nir
 
     return image_noglint
 
 
-
 class HedleySunGlint:
-    """ Hedley sun glint removal regression model
+    """Hedley sun glint removal regression model
 
     # Usage:
     GlintMod = HedleySunGlint()
@@ -191,10 +195,22 @@ class HedleySunGlint:
     Journal of Remote Sensing, 26(10), 2107–2112.
     https://doi.org/10.1080/01431160500034086
 
+    A very similar algorithm is described in Lyzenga, David R. & Malinas, Norman P.
+    & Tanis, Fred J. (2006) Multispectral bathymetry using a simple physically
+    based algorithm. IEEE Transactions on Geoscience and Remote Sensing , Vol. 44,
+    No. 8 p. 2251-2259
+    https://doi.org/10.1109/TGRS.2006.872909
+
+    The only difference between the two is that Hedley et al.
+    subtracts the minimum NIR signal observed, while Lyzenga et al. subtracts
+    the mean NIR signal. Hedley states clearly that the algorithm is based on
+    linear regression. Equations (4)-(6) in Lyzenga et al. express the same
+    mathematically, but it is not stated explicity that this is linear regression.
+
     """
 
-    def __init__(self, vis_band = (350,750), nir_band = (780,840)):
-        """ Hedley sun glint removal: Model instantiation
+    def __init__(self, vis_band=(350, 750), nir_band=(780, 840)):
+        """Hedley sun glint removal: Model instantiation
 
         # Optional arguments:
         vis_band:   2-element tuple with upper and lower wavelength of VIS band (nm).
@@ -205,9 +221,8 @@ class HedleySunGlint:
         self.vis_band = vis_band
         self.nir_band = nir_band
 
-
-    def fit(self,spec,wl):
-        """ Fit a linear regression model connecting NIR and VIS data
+    def fit(self, spec, wl):
+        """Fit a linear regression model connecting NIR and VIS data
 
         # Required arguments:
         spec:   2D numpy array with hyperspectral image, wavelengths along 2nd dim.
@@ -228,25 +243,24 @@ class HedleySunGlint:
         self.nir_ind = (wl >= self.nir_band[0]) & (wl <= self.nir_band[1])
 
         # Calculate data mask for valid (non-zero) data points
-        nz_mask = ~np.all(spec==0,axis=1)
+        nz_mask = ~np.all(spec == 0, axis=1)
 
         # Calculate mean NIR value in band
         # "Double indexing" needed for broadcasting to work
-        nir = np.mean(spec[nz_mask][:,self.nir_ind], axis=1, keepdims=True)
+        nir = np.mean(spec[nz_mask][:, self.nir_ind], axis=1, keepdims=True)
 
         # Extract VIS data
-        vis = spec[nz_mask][:,self.vis_ind]
+        vis = spec[nz_mask][:, self.vis_ind]
 
         # Fit a linear regression model
         reg = LinearRegression().fit(nir, vis)
-        self.b = np.reshape(reg.coef_,(1,-1))   # Row vector, for mat. mult.
+        self.b = np.reshape(reg.coef_, (1, -1))  # Row vector, for mat. mult.
 
         # Estimate minimum NIR value as 2nd percentile (more robust than strict minimum)
-        self.min_nir = np.percentile(nir,2)
+        self.min_nir = np.percentile(nir, 2)
 
-
-    def remove_glint(self,data,invalid_threshold=0.2):
-        """ Remove sun glint by applying previously trained model
+    def remove_glint(self, data, invalid_threshold=0.2):
+        """Remove sun glint by applying previously trained model
 
         # Required arguments:
         data:   2D or 3D spectral data, wavelengths along last dim.
@@ -266,17 +280,17 @@ class HedleySunGlint:
                 glint removed.
 
         # Notes:
-        The indices for the wavelengths in the returned 'vis' cube are 
+        The indices for the wavelengths in the returned 'vis' cube are
         stored as an attribute ('vis_ind') of the HedleySunGlint object.
         """
 
         # Shape into 2D array, save original shape for later
         input_shape = data.shape
-        data = np.reshape(data,(-1,data.shape[-1]))
+        data = np.reshape(data, (-1, data.shape[-1]))
 
         # Extract VIS and NIR bands
-        vis = data[:,self.vis_ind]
-        nir = np.mean(data[:,self.nir_ind], axis=1, keepdims=True)
+        vis = data[:, self.vis_ind]
+        nir = np.mean(data[:, self.nir_ind], axis=1, keepdims=True)
 
         # Offset NIR, taking into account "ambient" (minimum) NIR
         # NOTE: This may be unnecessary for UAV data (minimal atmosphere effects)?
@@ -284,14 +298,14 @@ class HedleySunGlint:
         nir[nir < 0] = 0
 
         # Estimate sun glint in VIS range and subtract it
-        vis = vis - nir @ self.b    # Matrix mult. with slope b for each VIS band
+        vis = vis - nir @ self.b  # Matrix mult. with slope b for each VIS band
 
         # Set negative values to zero (negative values are non-physical)
         vis[vis < 0] = 0
 
         # Reshape data to fit original dimensions
         output_shape = input_shape[:-1] + (vis.shape[-1],)
-        vis = np.reshape(vis,output_shape)
+        vis = np.reshape(vis, output_shape)
 
         # Set invalid pixels (too many zeros) to all-zeros
         zeros_fraction = np.count_nonzero(vis == 0, axis=2) / vis.shape[2]
@@ -307,6 +321,8 @@ class HedleySunGlint:
         return vis
 
 
-def filter_image_savitzky_golay(image,window_length=13,polyorder=3,axis=2):
-    """ Filter hyperspectral image using Savitzky-Golay filter with default arguments """
-    return savgol_filter(image,window_length=window_length,polyorder=polyorder,axis=axis)
+def filter_image_savitzky_golay(image, window_length=13, polyorder=3, axis=2):
+    """Filter hyperspectral image using Savitzky-Golay filter with default arguments"""
+    return savgol_filter(
+        image, window_length=window_length, polyorder=polyorder, axis=axis
+    )
